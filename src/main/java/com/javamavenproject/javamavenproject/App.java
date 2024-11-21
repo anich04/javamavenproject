e161466c7a11bdfdcffd05956db4eae6b8f7c950
anirudh <chennupatianirudh@gmail.com>
2024-11-21 15:52:36 +0530

    file changed

---
+++ src/main/java/com/javamavenproject/javamavenproject/App.java
@@ -0,0 +1,10 @@
+package com.javamavenproject.javamavenproject;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
